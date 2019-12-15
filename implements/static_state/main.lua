34d456a9411f90673f7bd500d94d069c1c8076f5
MaiHD <maihd.dev@gmail.com>
2019-12-15 22:30:42 +0700

    continue static state implements

--- implements/static_state/main.lua
+++ implements/static_state/main.lua
@@ -1,4 +1,29 @@
+local gameObject = {
+    animator = {
+        runAnimation = function (name, speed)
+            if name == "idle" then
+                
+            elseif name == "walk" then
 
+            else
+                error("unknown animation: " .. name)
+            end
+        end
+    },
+}
+
+local isChanged = false
+
+function love.update()
+    if isChanged then
+        isChanged = false
+
+        local animName = currentState.animation
+        local animator = gameObject.animator
+
+        animator.runAnimation(animName, currentState.animSpeed)
+    end
+end
 
 function love.draw()
     love.graphics.print("Static state implements")
