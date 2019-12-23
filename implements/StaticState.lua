1e0dd05fcc1c7f17d0d2b458d5e92943aafdb0e4
maihd <maihd.dev@gmail.com>
2019-12-23 14:50:12 +0700

    first animation done

--- implements/StaticState.lua
+++ implements/StaticState.lua
@@ -1,12 +1,21 @@
 local StaticState = {}
+local Animation = require("Animation")
+local Atlas = require("Atlas")
+
+local atlas = Atlas.load("assets/adventurer-Sheet.png", 11, 7)
 
 local gameObject = {
     animator = {
-        runAnimation = function (name, speed)
-            if name == "idle" then
-                
-            elseif name == "walk" then
+        animation = nil,
+        animSpeed = 0,
 
+        runAnimation = function (self, name, speed)
+            if name == "idle" then
+                self.animation = Animation.load(atlas, { 1, 2, 3, 4 }, 0.25)
+                self.animSpeed = speed
+            elseif name == "walk" then
+                self.animation = Animation.load(atlas, { 10, 11, 12, 13, 14 }, 0.2)
+                self.animSpeed = speed
             else
                 error("unknown animation: " .. name)
             end
@@ -26,10 +35,8 @@ local states = {
     },
 }
 
-local currentState = states.idle
-
+local currentState = nil
 local isChanged = false
-
 
 function idle(gameObject)
     if currentState ~= states.idle then
@@ -45,6 +52,10 @@ function walk(gameObject)
     end
 end
 
+function StaticState.load()
+    idle(gameObject)
+end
+
 function StaticState.keypressed(key)
     if key == "space" then
         if currentState == states.walk then
@@ -55,22 +66,25 @@ function StaticState.keypressed(key)
     end
 end
 
-function StaticState.update()
+function StaticState.update(dt)
     if isChanged then
         isChanged = false
 
         local animName = currentState.animation
         local animator = gameObject.animator
 
-        animator.runAnimation(animName, currentState.animSpeed)
+        animator:runAnimation(animName, currentState.animSpeed)
     end
+
+    Animation.update(gameObject.animator.animation, gameObject.animator.animSpeed * dt)
 end
 
 function StaticState.draw()
     love.graphics.print("Static state implements")
     love.graphics.print("Press SPACE to " .. (currentState == states.idle and "idle" or "walk"), 0, 16)
 
-    
+    local animation = gameObject.animator.animation
+    Animation.draw(animation, love.graphics.getWidth() * 0.5 - animation.width * 0.5, love.graphics.getHeight() * 0.5 - animation.height * 0.5, 0, 1.0, 1.0)
 end
 
 return StaticState
--- implements/main.lua
+++ implements/main.lua
@@ -1,9 +1,8 @@
 local StaticState = require("StaticState")
-local Animation = require("Animation")
-local Atlas = require("Atlas")
 
-local atlas = Atlas.load("assets/adventurer-Sheet.png", 11, 7)
-local animation = Animation.load(atlas, { 1, 2, 3, 4 }, 0.25)
+function love.load()
+    StaticState.load()
+end
 
 function love.keypressed(key, scancode)
     StaticState.keypressed(key, scancode)
@@ -11,10 +10,8 @@ end
 
 function love.update(dt)
     StaticState.update(dt)
-    Animation.update(animation, dt)
 end
 
 function love.draw()
     StaticState.draw()
-    Animation.draw(animation, love.graphics.getWidth() * 0.5 - animation.width * 0.5, love.graphics.getHeight() * 0.5 - animation.height * 0.5, 0, 1.0, 1.0)
 end
