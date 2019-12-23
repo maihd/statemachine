aa2e1bf98f094d60ad02fa6a6f24d5de274c15d0
maihd <maihd.dev@gmail.com>
2019-12-23 14:34:18 +0700

    refactory

--- implements/main.lua
+++ implements/main.lua
@@ -1,4 +1,7 @@
 local StaticState = require("StaticState")
+local Animation = require("Animation")
+
+local animation = Animation.load("adventurer-Sheet.png", 7, 11, 0.1)
 
 function love.keypressed(key, scancode)
     StaticState.keypressed(key, scancode)
@@ -6,8 +9,10 @@ end
 
 function love.update(dt)
     StaticState.update(dt)
+    Animation.update(animation, dt)
 end
 
 function love.draw()
     StaticState.draw()
+    Anmation.draw(animation)
 end
